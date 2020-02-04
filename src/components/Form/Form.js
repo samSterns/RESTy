06374e67ab5d19acd47c6195d475f21d356717fd
samSterns <sam.g.sterns@gmail.com>
2020-02-03 16:53:03 -0800

    little progress

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Form.css';
 
-const Form = ({ handleChange, url }) => {
+const Form = ({ handleChange, url, method }) => {
   return (
     <form className={styles.Form}>
       <aside> 
@@ -11,23 +11,23 @@ const Form = ({ handleChange, url }) => {
       </aside>
       <label className={styles.active}>
         <input className="url-style" type="text" id="url-text-area" name="url" placeholder="URL" value={url} onChange={handleChange}></input>
-        <input className="radio-button" type="radio" name="method" value="GET" onChange={handleChange}></input>
+        <input className="radio-button" type="radio" name="method" value="GET" checked={method === 'GET'} onChange={handleChange}></input>
         <span>GET</span>
       </label>
       <label>
-        <input className="radio-button" type="radio" name="method" value="POST" onChange={handleChange}></input>
+        <input className="radio-button" type="radio" name="method" value="POST" checked={method === 'POST'} onChange={handleChange}></input>
         <span>POST</span>
       </label>
       <label>
-        <input className="radio-button" type="radio" name="method" value="PUT" onChange={handleChange}></input>
+        <input className="radio-button" type="radio" name="method" value="PUT" checked={method === 'PUT'} onChange={handleChange}></input>
         <span>PUT</span>
       </label>
       <label>
-        <input className="radio-button" type="radio" name="method" value="PATCH" onChange={handleChange}></input>
+        <input className="radio-button" type="radio" name="method" value="PATCH" checked={method === 'PATCH'} onChange={handleChange}></input>
         <span>PATCH</span>
       </label>
       <label>
-        <input  className="radio-button" type="radio" name="method" value="DELETE" onChange={handleChange}></input>
+        <input  className="radio-button" type="radio" name="method" value="DELETE" checked={method === 'DELETE'} onChange={handleChange}></input>
         <span>DELETE</span>
         <div>
           <button className="go-button" type="submit" onChange={handleChange}>Go!</button>
@@ -40,7 +40,8 @@ const Form = ({ handleChange, url }) => {
 
 Form.propTypes = {
   handleChange: PropTypes.func.isRequired,
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired
 };
 
 export default Form; 
--- src/components/container/Resty.js
+++ src/components/container/Resty.js
@@ -8,34 +8,37 @@ export default class Resty extends Component {
     state = {
       url: '',
       jsoninput: '',
-      method: '',
-      response: '',
-      history: []
+      method: 'GET',
+      responseItem: {},
+      previousHistory: []
     }
-
-    handleChange = (event) => {
-      this.setState({ [event.target.name]: event.target.value });
-    }
-    onSubmit = event => {
-      event.preventDefault();
-  
-      this.setState(prevstate => ({
-        history: [...prevstate.history, {
-          method: this.state.method,
-          url: this.state.url,
-          jsoninput: this.state.url
-        }] 
-      }));
-      this.fetch();
-    };
 
     fetch = () => {
       return apiCall(this.state.url, this.state.method, this.state.jsoninput)
         .then(response => this.setState({ response: JSON.stringify(response, null, 2) }));
     }
 
+    handleChange = (event) => {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onSubmit = event => {
+      event.preventDefault();
+  
+      
+      this.setState(() => ({ previousHistory: [...previousHistory, {
+        method: this.state.method,
+        url: this.state.url,
+        jsoninput: this.state.url
+      }] 
+      }));
+      this.fetch();
+    };
+
+
     render() {
-        
+      const { previousHistory, url, method, reponseItem } = this.state;
+
       return (
         <>
           <Form 
